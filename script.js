26e02e1aaddaaef0efb4719eb80cc65e2eaa9b87
Kim Weast <weastkim3@gmail.com>
2023-10-20 11:59:08 -0500

    update

--- script.js
+++ script.js
@@ -1,96 +1,102 @@
-//Lets make a functions
-
-
-function nameOfFunction(waveDifficulty,a, b, c, something) {
-    return surferEnergy - waveDifficulty;
-
+//exercise 1 section
+//defined the function
+function printOdds(count){
+    //1. initilization
+    //2. Conditional
+    //3. incrementor
+    if (count > 0) {
+      for (let i =1; i <= count; i++) {
+        if(i % 2 == 1) {
+            console.log(i);
+        }
+    }
 }
-//invoke, caall use the function
-const result = calculateRemaingEnergy(6, 14);
-console.log(result);
+ else {  
+    for (let i = 0; i > count; i--){
+        if(i % 2 == 1) {
+            console.log(i);
+        }
+    }
+//to test the funcions,invoke it
+printOdds(10);
+printOdds(-33);
 
+//exercise 2 section
+function checkAge(userName, age ) { 
+        const aboveSixteen = 'Congrats ${userName}, you can drive'  ;
+        const belowSixteen = 'Sorry ${userName}, but you need to wait until you are 16.';
+        if(age < 16) {
+            console.log(belowSixteen);
+        } else if  (age >= 16 ){  
+            console.log(aboveSixteen);
+        } else { 
+            console.log("Invalid age");
+        }}}};
 
-function substract(a, b) {
-    return a - b;
-}
-const subResult = subtract(10, 4);
-console.log(subResult);
-
-function greetPerson(person){
-    console.log("Hello," + person + "!");
-}
-greetPerson("Lady");
-greetPerson("Hunter");
-greetPerson("Wesley");
-checkAge("", 15);
-
-//exercise 3
-function cartesian = (x, y) => { 
-        if (x == 0 && y == 0) { 
-            console.log("Origin Point");
-        } else if ( y == 0) { 
-            console.log("X-Axis") ;
-        }else if(x == 0){ 
-            console.log("Y-axis");
-        } else if (x > 0){ 
-            if(y > 0) { 
-                console.log("Quandrant 1");
-            }else{ 
-                console.log("Quandrant 4");
-            }
-            } else { 
-                if (y > 0){ 
-                    console.log("Quandrant 2");
-                } else{
-                    console.log("Quandrant 3");
-                 }
+        checkAge("Bilbo", 111);
+        checkAge("Ender" , 14);
+        checkAge("Ender", "pizza");
+        checkAge()
+      
+        //exercise 3
+        const caresian = (x, y) => {
+            if (x == 0 && y == 0){
+                console.log("Origin Point");
+            } else if  (y == 0) {
+                console.log("Y-Axis");
+            }else if  (x == 0) {
+                console.log ("Y-Axis");
+            } else  if (x > 0) { 
+                if (y > 0) {
+                    console.log("Quadrant 1");
+                } else { 
+                   console.log("Quadrant 4");
+                } else { 
+                    console.log("Quadrant 3");
                 }
-            }
-            //exercise 4
+            } };
+             //exercise 4
             const getTriangleType = (a, b, c) => {
-                if (a = b <= c  || a + c <= b || b + a <= c){
+                console.log(a, b, c);
+                if (a + b <= c  || a + c <= b || b + a <= c){
                    return "Invalide Triangle" ;
                 } else if (a == b && a == c) {
                     return "Equalatural";
-                } else {  
+                } else {  (a != b  && a != c && b != c) {
                     return "Scalene";
                 } else {  
-                   return "Isosceles";
-                }}
-               }
-               getTriangleType(1, 2, 2);
-               getTriangleType(1, 1, 2);
-               getTriangleType(2, 2, 2);
-               getTriangleType(3, 4, 5)
-
-               //Exercise 5
-               function dataPlan(planLimit, day, usage){
-                const daysRemaining = 30 - day;
-                const dataRemaing = planLimit - usage;
-                const idealUsageRemaining = (dataRemaining / daysRemaining);
-                const perfectAvg = planLimit / 30;
-                const currAvg = usage / day;
-                const projectTotalUsage = currAvg * 30;
-                const totalUsageDifference = planLImit-projectedTotalUsage;
-
-                //Plug in the variables into the paragraph
-            let output = `${day} days used, ${daysRemaining}days remaining.
-            Average daily use: ${perfectAvg} GB/day\n`;
-               
-
-               if(totalUsageDifference > 0)  {
-                //they are under their limit
-                output += `You are UNDER your average daily use(${currAvg})GB/day,
-                continuing this low usage, you will exceed your data plan by ${totalUsageDifference}GB of data unused.To maximize your remaining data, use $ {idealUsageRemainig}GB/day`;
-               } else if   (totalUsageDifference < 0) {
-                //they are over their limit
-                output += `You will be removed from our system.Your account has been flagged as a robot. You will be penalized and sentenced accordingly`;
-               
-                return output;
-
-
+                    return "Isoceles ";
               
-            }
-            console.log(dataPlan(100, 15, 45));
-            console.log(dtaPlan( 100, 15, 55));
-            console.log(dataPlan( 100,15,105));
+                }}}
+               
+              console.log (getTriangleType(1, 2, 2));
+               console.log(getTriangleType(1, 1, 2));
+               console.log(getTriangleType(2, 2, 2));
+               console.log(getTriangleType(3, 4, 5));
+
+
+          //exercise 5
+          function dataPlan(planLimit, day, usage) {
+            const daysRemainging = 30 - day;
+            const dataRemaning = planLimit - usage;
+            const projectedTotalUsage = currAvg * 30;
+            const totalUsageDifference = planLimit-projectTotalUsage;
+
+            //plug in the variable into the paragraph
+            let output = `${day}days used,${daysRemaining} days remaining/nAverage daily use: ${perfectAvg} GB/day\n`;
+
+            if (usage >= planLimit) {
+                output += `You have used ALL of your alloted data for the month. You will be charged $700/GB of extra data used. Please keep using data , we enjoy taking your money\n If you keep up this package, you will owe $$ {projectedTotalUsage * 700} by the end of the month.` 
+            }`${day} days used, ${daysRemaining} days remaining
+             ${idealUsageRemaing} GB/day.` 
+        }
+        return output;
+
+        console.log(dataPlan(100, 15, 56));
+
+            
+            
+            
+
+            
+
